df427b6d4eb030a744b9b2c188cbcef90057d9d7
rainyear <sniky.hack@gmail.com>
2016-12-20 00:50:37 +0800

    test github x-header

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, json
 from flask_sqlalchemy import SQLAlchemy
 import os
+import hashlib
+import sys
 
 app = Flask(__name__)
 app.config.from_pyfile('app.cfg')
@@ -24,5 +26,13 @@ def issue_page(issue_id=''):
     return issue_id
 @app.route('/wh/github', methods=['POST'])
 def webhook_github():
-    print(request.data)
-    return json.loads(request.data)
+    if request.headers['X-GitHub-Event'] == 'push':
+        os.system("git pull")
+    print(request.headers)
+    print(verify_webhook_signature(request.headers['X-Hub-Signature'], request.data))
+    return "OK"
+
+def verify_webhook_signature(sig, payload):
+    sha = hashlib.sha1(GITHUB_SEC + payload)
+
+    return sig == 'sha1=' + sha.hexdigest()
